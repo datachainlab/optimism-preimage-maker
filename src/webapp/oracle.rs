536393fb5eb4447504e582308b0f922939fa8cd6
yoshidan <naohiro.y@gmail.com>
2024-12-18 11:03:44 +0900

    share module

--- src/webapp/oracle.rs
+++ src/webapp/oracle.rs
@@ -1,16 +1,13 @@
-use crate::derivation::oracle::Cache;
 use alloy_primitives::{keccak256, B256};
 use hashbrown::HashMap;
 use kona_preimage::errors::PreimageOracleResult;
 use kona_preimage::{CommsClient, HintWriterClient, PreimageKey, PreimageKeyType, PreimageOracleClient};
-use lru::LruCache;
-use prost::Message;
 use spin::Mutex;
 use std::fmt::Debug;
 use std::sync::Arc;
 use alloy_eips::eip4844::FIELD_ELEMENTS_PER_BLOB;
-use log::info;
 use optimism_derivation::POSITION_FIELD_ELEMENT;
+use optimism_derivation::types::{Preimage, Preimages};
 
 #[derive(Debug, Clone)]
 pub struct TracingPreimageIO<T>
@@ -78,9 +75,7 @@ where
             temp.push(Preimage::new(k.clone(), v.clone()));
         }
         let data = Preimages { preimages: temp };
-        let mut buf: Vec<u8> = Vec::new();
-        data.encode(&mut buf).unwrap();
-        buf
+        data.into_vec().unwrap()
     }
 }
 
@@ -109,68 +104,5 @@ where
 {
     async fn write(&self, hint: &str) -> PreimageOracleResult<()> {
         self.inner.write(hint).await
-    }
-}
-
-#[derive(::prost::Message, Clone, PartialEq)]
-pub struct Preimage {
-    #[prost(bytes = "vec", tag = "1")]
-    pub key: Vec<u8>,
-    #[prost(bytes = "vec", tag = "2")]
-    pub data: Vec<u8>,
-}
-
-impl Preimage {
-    pub fn new(key: PreimageKey, data: Vec<u8>) -> Self {
-        Self {
-            key: B256::from(key).0.to_vec(),
-            data,
-        }
-    }
-}
-
-#[derive(::prost::Message, Clone, PartialEq)]
-pub struct Preimages {
-    #[prost(message, repeated, tag = "1")]
-    pub preimages: Vec<Preimage>,
-}
-
-#[cfg(test)]
-mod test {
-    use crate::webapp::oracle::{Preimage, Preimages};
-    use prost::Message;
-
-    #[test]
-    pub fn test_preimage_encode_decode() {
-        let expected = Preimage {
-            key: vec![1, 2, 3],
-            data: vec![4, 5, 6],
-        };
-        let mut buf: Vec<u8> = Vec::new();
-        expected.encode(&mut buf).unwrap();
-
-        let actual = Preimage::decode(&*buf).unwrap();
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    pub fn test_preimages_encode_decode() {
-        let expected = Preimages {
-            preimages: vec![
-                Preimage {
-                    key: vec![1, 2, 3],
-                    data: vec![4, 5, 6],
-                },
-                Preimage {
-                    key: vec![7, 8, 9],
-                    data: vec![10, 11, 12],
-                },
-            ],
-        };
-        let mut buf: Vec<u8> = Vec::new();
-        expected.encode(&mut buf).unwrap();
-
-        let actual = Preimages::decode(&*buf).unwrap();
-        assert_eq!(expected, actual);
     }
 }
